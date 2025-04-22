e75dd092932bc51a86e881e6d95f58f579dc4c1b
Abel Moran <a_moran2@uncg.edu>
2025-04-22 02:39:11 -0400

    Removed Old Extra files for Organization

---
+++ src/main/java/com/aniwatch/aniwatch/anime/AnimeRepository.java
@@ -0,0 +1,8 @@
+package com.aniwatch.aniwatch.anime;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+}
